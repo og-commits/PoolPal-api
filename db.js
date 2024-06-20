a750ce3ac1126a6f8e2b7962cb2afef1df0ada83
gupta6OO7 <guptaom6007@gmail.com>
2024-06-20 22:50:20 +0530

    initial commit

---
+++ db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
+
+const mongoDb = async() => {
+    const uri = 'mongodb+srv://og:' + process.env.DB_PASSWORD + '@cluster0.g6omsii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+    await mongoose.connect(uri, async() => {
+        console.log("connected");
+    });
+}
+
+module.exports = mongoDb;
